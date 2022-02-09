696cdf36187e17f5b6c4ed419b4626b64b44fed4
andjsrk <43586090+andjsrk@users.noreply.github.com>
2022-02-10 08:29:42 +0900

    Change else to return

--- src/main.rs
+++ src/main.rs
@@ -20,46 +20,38 @@ fn main() {
 		let readline_result = std::io::stdin().read_line(&mut input);
 		input = String::from(input.trim());
 		if readline_result.is_ok() {
-			if input.chars().count() < 2 {
-				println!("단어의 길이는 2자 이상이어야 합니다.");
-				continue;
-			} else {
-				if input == "exit()" {
-					break;
-				} else {
-					println!("입력한 단어: {}", input);
-					let first_char: char = input.chars().nth(0).unwrap();
-					if starting_char.is_some() && first_char != starting_char.unwrap() {
-						println!("{}는 {}로 시작하지 않습니다.", first_char, starting_char.unwrap());
-						continue;
-					}
-					let found_words = dict.get(&first_char);
-					if found_words.is_some() {
-						if found_words.unwrap().contains(&&*input) {
-							let last_char = input.chars().last().unwrap();
-							let continuable_words = dict.get(&last_char);
-							if continuable_words.is_none() {
-								println!("와 한방단어");
-								break;
-							} else {
-								if input_words.contains(&input) {
-									println!("{}은(는) 이미 사용된 단어입니다.", input);
-									continue;
-								} else {
-									input_words.push(input.clone());
-									starting_char = Some(last_char);
-								}
-							}
-						} else {
-							println!("{}? 그런 건 없다.", input);
-							continue;
-						}
-					} else {
-						println!("{}? 그런 건 없다.", input);
-						continue;
-					}
-				}
-			}
+			println!("입력 받기 실패");
+			continue;
 		}
+		if input.chars().count() < 2 {
+			println!("단어의 길이는 2자 이상이어야 합니다.");
+			continue;
+		}
+		if input == "exit()" {
+			break;
+		}
+		println!("입력한 단어: {}", input);
+		let first_char: char = input.chars().nth(0).unwrap();
+		if starting_char.is_some() && first_char != starting_char.unwrap() {
+			println!("{}는 {}로 시작하지 않습니다.", first_char, starting_char.unwrap());
+			continue;
+		}
+		let found_words = dict.get(&first_char);
+		if !found_words.is_some() || !found_words.unwrap().contains(&&*input) {
+			println!("{}? 그런 건 없다.", input);
+			continue;
+		}
+		let last_char = input.chars().last().unwrap();
+		let continuable_words = dict.get(&last_char);
+		if continuable_words.is_none() {
+			println!("와 한방단어");
+			break;
+		}
+		if input_words.contains(&input) {
+			println!("{}은(는) 이미 사용된 단어입니다.", input);
+			continue;
+		}
+		input_words.push(input.clone());
+		starting_char = Some(last_char);
 	}
 }
